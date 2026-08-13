5c6ace79ebf65714ce0aafeadfbfed32598a6fa5
Subham Kar <karsubham669@gmail.com>
2026-08-13 12:26:41 +0530

    Add Chapter 8 practice set - strings (Q1-Q9)
    Encrypt a string by adding 1 to ASCII value of each character

---
+++ 06_problem6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[99];
+    printf("Enter your srting: \n");
+    scanf("%s", str);
+    for (int i = 0; i < strlen(str); i++)
+    {
+        str[i] = str[i] + 1;
+    }
+    printf("%s \n", str);
+
+    return 0;
+}
